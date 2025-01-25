fbc666904c2615ea5d28df14c53fa7cce57b9348
Lucas Quintela <lucasquintela.dev@gmail.com>
2025-01-25 17:24:11 -0300

    Operadores e funções

---
+++ javascripBasic1/8.IntroducaoFunçoes/introducaoFuncoes.js
@@ -0,0 +1,12 @@
+/**
+ *  Funções em JavaScript (Básico)
+ * Funções são blocos de código reutilizáveis que executam uma tarefa específica.
+ * Elas ajudam a organizar, reutilizar e reduzir a repetição de código.
+ * 
+ * Estrutura Básica de uma Função
+    function nomeDaFuncao(parametro1, parametro2) {
+    // Código que será executado
+    return resultado; // (Opcional) Retorna um valor
+    }
+
+ */
